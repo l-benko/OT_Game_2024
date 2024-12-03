37c9bd28a7f943f0cf698f5b4f94abd21949934d
sasu4 <lubomir.benko@gmail.com>
2024-12-03 19:28:27 +0100

    Adding map objects into the game

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 from settings import *
 from pytmx.util_pygame import load_pygame
 from sprites import *
+from player import Player
 
 
 class Game:
@@ -12,7 +13,9 @@ class Game:
         pygame.display.set_caption("OT Game")
         self.clock = pygame.time.Clock()
         self.running = True
+        # groups
         self.all_sprites = pygame.sprite.Group()
+        self.collision_sprites = pygame.sprite.Group()
         self.setup_map()
 
 
@@ -20,6 +23,14 @@ class Game:
         map = load_pygame(join('assets', 'map', 'level.tmx'))
         for x, y, image in map.get_layer_by_name('terrain').tiles():
             Sprite((x*TILE_SIZE, y*TILE_SIZE), image, self.all_sprites)
+        for item in map.get_layer_by_name('objects non'):
+            Sprite((item.x, item.y), item.image, self.all_sprites)
+        for item in map.get_layer_by_name('objects'):
+            if item.image is not None:
+                Sprite((item.x, item.y), item.image, (self.all_sprites, self.collision_sprites))
+        for item in map.get_layer_by_name('entities'):
+            if item.name == 'Player':
+                self.player = Player((item.x, item.y), self.all_sprites, self.collision_sprites)
 
     def run(self):
         while self.running:
