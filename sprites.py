eb341239804d42ad3652a7f293c86c28db21e5c5
sasu4 <lubomir.benko@gmail.com>
2024-12-07 22:17:38 +0100

    Enemy bugs issue solve

--- sprites.py
+++ sprites.py
@@ -23,6 +23,7 @@ class Enemy(pygame.sprite.Sprite):
         # image
         self.frames = frames
         self.frame_index = 0
+        self.image = self.frames[self.frame_index]
         self.animation_speed = 6
         # rectangle
         self.rect = self.image.get_frect(center = position)
@@ -33,13 +34,15 @@ class Enemy(pygame.sprite.Sprite):
 
     def animate(self, delta):
         self.frame_index += self.animation_speed * delta
-        self.image = self.frames[self.frame_index % len(self.frames)]
+        self.image = self.frames[int(self.frame_index) % len(self.frames)]
 
     def move(self, delta):
         player_position = pygame.Vector2(self.player.rect.center)
         enemy_position = pygame.Vector2(self.rect.center)
+        print(self.direction)
         self.direction = (player_position - enemy_position).normalize()
-        self.hitbox_rect = self.direction * self.speed * delta
+        self.hitbox_rect.x = self.direction.x * self.speed * delta
+        self.hitbox_rect.y = self.direction.y * self.speed * delta
         self.rect.center = self.hitbox_rect.center
 
     def update(self, delta):
