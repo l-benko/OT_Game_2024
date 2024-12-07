b7c51f39e7c206e29505125107635c102ffbcedf
sasu4 <lubomir.benko@gmail.com>
2024-12-07 21:38:27 +0100

    Creating Enemy class with movement and animate

--- sprites.py
+++ sprites.py
@@ -14,3 +14,34 @@ class CollisionSprite(pygame.sprite.Sprite):
         super().__init__(groups)
         self.image = surface
         self.rect = self.image.get_rect(topleft=position)
+
+
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self, position, frames, groups, player, collision_sprites):
+        super().__init__(groups)
+        self.player = player
+        # image
+        self.frames = frames
+        self.frame_index = 0
+        self.animation_speed = 6
+        # rectangle
+        self.rect = self.image.get_frect(center = position)
+        self.hitbox_rect = self.rect.inflate(-20, -40)
+        self.collision_sprites = collision_sprites
+        self.direction = pygame.Vector2()
+        self.speed = 150
+
+    def animate(self, delta):
+        self.frame_index += self.animation_speed * delta
+        self.image = self.frames[self.frame_index % len(self.frames)]
+
+    def move(self, delta):
+        player_position = pygame.Vector2(self.player.rect.center)
+        enemy_position = pygame.Vector2(self.rect.center)
+        self.direction = (player_position - enemy_position).normalize()
+        self.hitbox_rect = self.direction * self.speed * delta
+        self.rect.center = self.hitbox_rect.center
+
+    def update(self, delta):
+        self.move(delta)
+        self.animate(delta)
